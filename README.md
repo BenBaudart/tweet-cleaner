f195d31b0a8d3f106d46475348bbc3e7704c69c0
Ben Baudart <benbaudart@stereoagency.com>
2019-01-02 19:09:39 +0100

    - Updated code & dependencies - Date passed as a parameter in the process command - Verbose output

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# tweet-cleaner
-Node.js module for deleting tweets
+#Old tweets cleaner
+###Node.js module for deleting tweets based on a .csv archive file from Twitter
+
+Specify a date, and watch all the tweets older than this date get deleted. 
+
+--
+
+**1 - Install this script** 
+--
+Node.js must be already installed on your system
+
+```
+$ cd your/project/directory
+```
+> Go into the working project directory and run the following:
+
+
+```
+$ npm install
+```
+> This will install all necessary packages / dependencies in the project
+
+
+**2 - Adjust your settings**
+--
+
+* Edit the file "credentials-template.js" with your [Twitter App tokens](https://developer.twitter.com/en/apps)
+* Save it as "credentials.js" in the "conf" folder
+* Copy your tweets.csv file into the "files" folder
+
+
+**3 - Start the process**
+--
+
+###WARNING:###
+The script will NOT ask for an extra confirmation.
+If tweets are matching your query, they WILL BE DELETED right away. So remember this: "Measure twice, cut once!"
+
+> Run the script with:
+
+```
+$ node clean.js 2018-12-31
+```
+And don't forget to specify the date!
+
+
+**Results**
+--
+
+> Results will be displaying something like this:
+
+```
+Starting Operations ----------
+Considering deletion of tweets older than 2018-12-31
+
+Building the list of tweets
+99 tweets added / 666 tweets discarded
+
+Trying to delete tweet: 0001112223334445678
+Successfully deleted tweet 1 with id: 0001112223334445678
+
+>> [Looping through individual tweets here]
+
+Operations Finished ----------
+Total tweets deleted: 99
+
+```
+
+
+
+## Credits
+
+This is a fork of the [original project from kakts](https://github.com/kakts/tweet-cleaner). It's also inspired by the fork of [Sujal](https://github.com/sujal/tweet-cleaner).
--- clean.js
+++ clean.js
@@ -1,52 +1,92 @@
 const Twitter = require('twitter');
 const _ = require('lodash');
 const async = require('neo-async');
-const config = require('./conf/credential');
+const config = require('./conf/credentials');
 const csv = require('csvtojson');
 
-// Create twitter client instance.
+// Create Twitter client instance
 const client = new Twitter(config);
 
-// Please set you latest date what you want to delete.
 // This script delete user tweets in descending date order.
 // It can delete max 3200 tweets one time.
-const finalDate = "2017-04-01";
-const finalTimestamp = Date.parse(finalDate);
+
 
 const MAX_API_REQUEST_COUNT = 3200;
 
 let list = [];
 let deleteCount = 0;
+let lastTweetId = 0;
 
-console.log('------Start deleting.-----');
+let tweetAdded = 0;
+let tweetNotAdded = 0;
+
+
+if (process.argv.length <= 2) {
+    console.log("Missing argument: date");
+    console.log("Correct syntax is: \"node clean.js 2018-12-31\"");
+    process.exit(-1);
+}
+
+const limitDate = process.argv[2];
+const finalTimestamp = Date.parse(limitDate);
+
+console.log('\n' + 'Starting Operations ----------' + '\n');
+
 csv()
-  .fromFile('./tweets.csv')
+  .fromFile('./files/tweets.csv')
   .on('json',(jsonObj)=>{
     const tweetTimestamp = Date.parse(jsonObj.timestamp);
     if (finalTimestamp > tweetTimestamp) {
       list.push(jsonObj.tweet_id);
+      tweetAdded+=1;
+    } else {
+      tweetNotAdded+=1;
     }
   })
   .on('done',(error)=>{
+    console.log('Considering deletion of tweets older than ' + limitDate);
+    console.log('\nBuilding the list of tweets');
+    console.log(tweetAdded + ' tweets added / ' + tweetNotAdded + ' tweets discarded');
+
     async.eachSeries(list, (tweetId, next) => {
       // If the deleteCount exceeds MAX_API_REQUEST_COUNT, it will be skipped.
       if (deleteCount > MAX_API_REQUEST_COUNT) {
+        console.error("Maximum API limit reached ----------");
         return next();
       }
       // Delete specified tweets.
+      console.error('\n' + "Trying to delete tweet: " + tweetId);
       client.post('statuses/destroy/' + tweetId, (err, result, response) => {
         if (err) {
           return next(err);
         }
         deleteCount++;
-        console.log('success: tweetId: ', tweetId, deleteCount);
+        lastTweetId = tweetId;
+        console.log('Successfully deleted tweet ' + deleteCount + " with id: " + tweetId, );
         next();
       });
     }, (err, res) => {
+      if (res) {
+        console.error("Res ----------");
+        console.error(res);
+        // console.error("code" + err[code]);
+      }
+
+        console.log('\n' + 'Operations Finished ----------');
+        console.log('Total tweets deleted: ' + deleteCount);
+        if (deleteCount > 0) { console.log('Last tweet has id: ' + lastTweetId);}
+        console.log('\n');
+
       if (err) {
+        console.error("Error ----------");
         console.error(err);
+        // console.error("code" + err[code]);
         return;
       }
-      console.log('finish. The last delete tweetId: ' + tweetId + '\n');
+
     });
+
+
+
   });
+
